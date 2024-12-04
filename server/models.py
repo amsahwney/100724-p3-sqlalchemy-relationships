2f8b4c9c043770733516b78485ede3ba1fc752b7
Amrita <amritak.saw@gmail.com>
2024-12-04 10:49:09 -0500

    lesson done

--- server/models.py
+++ server/models.py
@@ -15,9 +15,27 @@ class Deli(db.Model):
     name = db.Column(db.String)
     has_bodega_cat = db.Column(db.Boolean)
 
+    hamburgers = db.relationship('Hamburger', back_populates='deli')
+    pizzas = db.relationship('Pizza', back_populates='deli')
+
 class Hamburger(db.Model):
 
     __tablename__ = "hamburgers_table"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    deli_id = db.Column(db.Integer, db.ForeignKey('delis_table.id'))
+
+    deli = db.relationship('Deli', back_populates='hamburgers')
+
+class Pizza(db.Model):
+    __tablename__ = "pizza_table"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    deli_id = db.Column(db.Integer, db.ForeignKey('delis_table.id'))
+
+    deli = db.relationship('Deli', back_populates='pizzas')
+
